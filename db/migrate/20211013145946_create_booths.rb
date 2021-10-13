a73595e3ab27172b10bd8233b51422dcad3ccf62
pipipik <hiro.konan913@gmail.com>
2021-10-14 00:02:30 +0900

    マイグレーションファイル編集

--- db/migrate/20211013145946_create_booths.rb
+++ db/migrate/20211013145946_create_booths.rb
@@ -1,7 +1,10 @@
 class CreateBooths < ActiveRecord::Migration[6.0]
   def change
     create_table :booths do |t|
-
+      t.text :prize_name
+      t.text :machine_name
+      t.text :category
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
