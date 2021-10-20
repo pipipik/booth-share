8d900c0c19ebb9463cc865ee057e4a42baac465e
pipipik <hiro.konan913@gmail.com>
2021-10-20 22:45:48 +0900

    アソシエーション設定

--- app/models/booth.rb
+++ app/models/booth.rb
@@ -6,4 +6,5 @@ class Booth < ApplicationRecord
   
   belongs_to :user
   has_one_attached :image
+  has_many :comments
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,4 @@
 class Comment < ApplicationRecord
+  belongs_to :booth
+  belongs_to :user
 end
