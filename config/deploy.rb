3c98c3bcb1bf246e5c02101c7ccef763d5987199
pipipik <hiro.konan913@gmail.com>
2021-10-20 11:24:05 +0900

    自動デプロイ

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+lock '3.16.0'
+
+set :application, 'booth-share'
+
+set :repo_url,  'git@github.com:pipipik/booth-share.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/p79qdsna.pem'] 
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
