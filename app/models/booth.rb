d714fd971d79e19738dcd9d0ae92b6f8bb47f4cf
pipipik <hiro.konan913@gmail.com>
2021-11-17 18:44:48 +0900

    モデル編集

--- app/models/booth.rb
+++ app/models/booth.rb
@@ -1,7 +1,7 @@
 class Booth < ApplicationRecord
 
   with_options presence: true do
-    validates :image, :prize_name, :machine_name, :category
+    validates :image, :prize_name, :machine_name, :category, :price
   end
   
   belongs_to :user
